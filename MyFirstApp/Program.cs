f1b758c03da7988b3a374722d80cd90a58d37ea9
Janela Pal <janelapalmiste@gmail.com>
2022-09-07 12:50:47 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+
+//Rakendus küsib sisestajalt tema nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string- sõna
+string userName = Console.ReadLine(); //Input
+Console.WriteLine("Hello" + ", "+ userName + "!"); //Output
+// string interpolation
+
+Console.WriteLine($"Hello, {userName}!" ); //Output
